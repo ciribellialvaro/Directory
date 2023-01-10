0026eee13689bfb92fcb6f84811ec402f69144c0
Álvaro Borges <gestor.alvaro@gmail.com>
2023-01-09 23:00:35 -0300

    Add project files.

---
+++ Directory/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            //var files = Directory.GetFiles(@"c:\CS50", "*.*", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //    Console.WriteLine(file);
+
+            var directories = Directory.GetDirectories(@"c:\CS50", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+                Console.WriteLine(directory);
+
+
+            Directory.Exists("...");
+
+        }
+    }
+}
